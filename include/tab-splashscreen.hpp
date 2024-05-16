3d4e23f6d7156d4fac21c12f0f70a3271a93d549
JLatex <89390295+jlatex@users.noreply.github.com>
2024-05-16 13:52:28 +0100

    Make it an icon bar, we can show wifi and bluetooth scanning status too

--- include/tab-splashscreen.hpp
+++ include/tab-splashscreen.hpp
@@ -18,6 +18,7 @@ class tab_splashscreen: public Tab {
     private:
      unsigned long batterycheckmillis = 0;
      const char *batteryicons[6] = {LV_SYMBOL_BATTERY_EMPTY, LV_SYMBOL_BATTERY_1, LV_SYMBOL_BATTERY_2, LV_SYMBOL_BATTERY_3, LV_SYMBOL_BATTERY_FULL, LV_SYMBOL_CHARGE};
-     lv_obj_t *labelbattery;
+     lv_obj_t *labelicons;
+     void updateicons(void);
 };
 
