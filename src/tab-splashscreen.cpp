6aaf32f8bf168ba1d1f6a9d472008a5673c74cdd
JLatex <89390295+jlatex@users.noreply.github.com>
2024-05-16 13:49:04 +0100

    remove debugging

--- src/tab-splashscreen.cpp
+++ src/tab-splashscreen.cpp
@@ -57,32 +57,19 @@ void tab_splashscreen::loop(boolean activetab) {
     int level = min(4,M5.Power.getBatteryLevel() / 20);
     lv_label_set_text_fmt(labelbattery, "%s%s",batteryicons[level], M5.Power.isCharging()?batteryicons[5]:"");
   }
-
 }
-
-tab_object_timer *ttest;
-
 
 void tab_splashscreen::switch_change(int sw, boolean value) {
   ESP_LOGI("splashscreen", "new callback button %d %s\n", sw,
            value ? "push" : "release");
   if (sw == 4 && value) {
-    printf_log("Battery %dmV level %d%%\n", M5.Power.getBatteryVoltage(), M5.Power.getBatteryLevel());
-    if (M5.Power.isCharging()) {
-      printf_log("Charging\n");
-    }
-    //printf_log("%d=%d\n",M5.getBoard(),lgfx::boards::board_M5StackCoreS3);
     dump_connected_devices();
-  }
-  if (sw == 3 && value && ttest != nullptr) {
-    ttest->highlight_next_field();
   }
   if (value) buttonhue[sw] = 0;
 }
 
 void tab_splashscreen::encoder_change(int sw, int change) {
   ESP_LOGI("splashscreen", "Encoder %d: %+d\n", sw, change);
-  if (sw == 3 && ttest != nullptr) ttest->rotary_change(change);
 }
 
 void tab_splashscreen::setup(void) {
@@ -104,10 +91,6 @@ void tab_splashscreen::setup(void) {
   lv_obj_set_style_text_align(labelbattery, LV_TEXT_ALIGN_RIGHT, 0);
   lv_obj_align(labelbattery, LV_ALIGN_TOP_RIGHT, -8, 0);
   lv_obj_set_style_text_font(labelbattery, &lv_font_montserrat_24, LV_PART_MAIN);
-
-  ttest = nullptr;
-  // t = new tab_object_timer(false); //just for testing!
-  // t->view(tv2);
 
   page = tv2;
 }
