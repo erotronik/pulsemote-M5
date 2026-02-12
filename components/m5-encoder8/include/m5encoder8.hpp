75b8e7d1f2d49fe73a576831a0ab55dbaf97e06e
JLatex <89390295+jlatex@users.noreply.github.com>
2026-02-12 15:53:30 +0000

    Add m5encoder8 blank lock to older m5unified as 'develop' sometimes breaks

---
+++ components/m5-encoder8/include/m5encoder8.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <M5Unified.h>
+
+class M5Encoder8 {
+public:
+    M5Encoder8();
+    ~M5Encoder8();
+    
+    void init();
+    void update();
+    
+private:
+};
---
+++ components/m5-encoder8/src/m5encoder8.cpp
@@ -0,0 +1,13 @@
+#include "m5encoder8.hpp"
+
+M5Encoder8::M5Encoder8() {
+}
+
+M5Encoder8::~M5Encoder8() {
+}
+
+void M5Encoder8::init() {
+}
+
+void M5Encoder8::update() {
+}
