0d42744f3ab6696483db690ca7a0656067a9aaa1
JLatex <89390295+jlatex@users.noreply.github.com>
2026-03-14 07:20:14 +0000

    Add ability to clamp a timer with a maximum time

--- main/ui/widgets/tab-object-timer.cpp
+++ main/ui/widgets/tab-object-timer.cpp
@@ -3,17 +3,40 @@
 #include "lvgl-utils.h"
 #include "tab-object-timer.hpp"
 
-tab_object_timer::tab_object_timer(bool irandom) { moderandom = irandom; }
+static void sanitize_lv_objects(lv_obj_t *&container, lv_obj_t *&active_btn, bool &is_visible) {
+  if (container && !lv_obj_is_valid(container)) {
+    container = nullptr;
+    active_btn = nullptr;
+    is_visible = false;
+    return;
+  }
+  if (active_btn && !lv_obj_is_valid(active_btn)) {
+    active_btn = nullptr;
+  }
+}
 
-bool tab_object_timer::has_focus(void) { return (active_btn  && container && is_visible); }
+tab_object_timer::tab_object_timer(bool irandom, bool ion_only) {
+  moderandom = irandom;
+  mode_on_only = ion_only;
+  active_btn = nullptr;
+  container = nullptr;
+  is_visible = false;
+}
+
+bool tab_object_timer::has_focus(void) {
+  sanitize_lv_objects(container, active_btn, is_visible);
+  return (active_btn  && container && is_visible);
+}
 
 void tab_object_timer::rotary_change(int change) {
+  sanitize_lv_objects(container, active_btn, is_visible);
   if (!container) return;
   if (!active_btn) return;
   if (!is_visible) return;
 
   uint32_t *id_ptr = (uint32_t *)lv_obj_get_user_data(active_btn);
   int32_t id = *id_ptr - 1;
+  if (mode_on_only && id != 0) return;
   value[id] = std::max<int>(1, value[id]+change);
   if (moderandom) {
     // special cases so ranges make sense
@@ -22,16 +45,29 @@ void tab_object_timer::rotary_change(int change) {
     if (id == 2) value[3] = std::max(value[2], value[3]);
     if (id == 3) value[2] = std::min(value[2], value[3]);
   }
-  lv_obj_t *btns = container;
-  int j = 4;
-  if (!moderandom) j = 2;
-  for (int i = 0; i < j; i++) {
-    lv_obj_t *btnn = lv_obj_get_child(btns, i);
-    if (btnn) lv_label_set_text_fmt(lv_obj_get_child(btnn, 0), "%d", value[i]);
+  // Clamp ON time(s) to max_on
+  if (max_on > 0) {
+    value[0] = std::min(value[0], max_on);
+    if (moderandom) value[1] = std::min(value[1], max_on);
+  }
+  uint32_t child_count = lv_obj_get_child_count(container);
+  for (uint32_t i = 0; i < child_count; i++) {
+    lv_obj_t *btnn = lv_obj_get_child(container, i);
+    if (!btnn) continue;
+    if (!lv_obj_has_flag(btnn, LV_OBJ_FLAG_CHECKABLE)) continue;
+
+    uint32_t *btn_id_ptr = (uint32_t *)lv_obj_get_user_data(btnn);
+    if (!btn_id_ptr || *btn_id_ptr == 0) continue;
+    int32_t btn_id = *btn_id_ptr - 1;
+    if (btn_id < 0 || btn_id >= 5) continue;
+
+    lv_obj_t *label = lv_obj_get_child(btnn, 0);
+    if (label) lv_label_set_text_fmt(label, "%d", value[btn_id]);
   }
 }
 
 bool tab_object_timer::highlight_next_field() {
+  sanitize_lv_objects(container, active_btn, is_visible);
   if (!container) return false;
   if (!is_visible) return false;
   if (!active_btn) {
@@ -41,7 +77,7 @@ bool tab_object_timer::highlight_next_field() {
     uint32_t *id_ptr = (uint32_t *)lv_obj_get_user_data(active_btn);
     int32_t id = *id_ptr;
     lv_obj_clear_state(active_btn, LV_STATE_CHECKED);
-    if ((moderandom && id > 3) || (!moderandom && id > 1)) {
+    if ((moderandom && id > 3) || (mode_on_only && id > 0) || (!moderandom && id > 1)) {
       active_btn = NULL;
     } else {
       active_btn = lv_obj_get_child(container, id);  // null is okay for last one
@@ -76,6 +112,7 @@ void tab_object_timer::event_handler(lv_event_t *e) {
 }
 
 void tab_object_timer::show(bool show) {
+  sanitize_lv_objects(container, active_btn, is_visible);
   if (container && show) {
     lv_obj_clear_flag(container, LV_OBJ_FLAG_HIDDEN);
     is_visible = true;
@@ -93,6 +130,7 @@ int tab_object_timer::gettimeon(void) {
 }
 
 int tab_object_timer::gettimeoff(void) {
+  if (mode_on_only) return 0;
   if (moderandom)
     return value[2] + rand() % (value[3] - value[2]);
   else
@@ -121,13 +159,15 @@ void tab_object_timer::view(lv_obj_t *tv2) {
   lv_obj_t *timerc = lv_obj_create(tv2);
   lv_obj_align(timerc, LV_ALIGN_TOP_RIGHT, 0, LV_SCALE(dropdown_height+6));
   lv_obj_set_style_pad_all(timerc, LV_SCALE(3), LV_PART_MAIN);
-  lv_obj_set_size(timerc, LV_SCALE(dropdown_width), LV_SCALE(76));
+  lv_obj_set_size(timerc, LV_SCALE(dropdown_width), LV_SCALE(mode_on_only ? 42 : 76));
 
   active_btn = NULL;
   container = timerc;
 
   if (moderandom) {
     value[0] = 5; value[1] = 10; value[2] = 20; value[3] = 30;
+  } else if (mode_on_only) {
+    value[0] = 5;
   } else {
     value[0] = 5; value[1] = 10;
   }
@@ -151,6 +191,16 @@ void tab_object_timer::view(lv_obj_t *tv2) {
     lv_label_set_text(t2, "Off");
     lv_obj_set_style_text_align(t2, LV_TEXT_ALIGN_CENTER, 0);
     lv_obj_set_style_text_font(t2, LV_FONT_GET(14), 0);
+  } else if (mode_on_only) {
+    lv_obj_t *btn = make_grid_btn(timerc, LV_ALIGN_RIGHT_MID, value[0], &ids[0], this, event_handler);
+    lv_obj_align(btn, LV_ALIGN_RIGHT_MID, 0, LV_SCALE(1));
+
+    lv_obj_t *t1 = lv_label_create(timerc);
+    lv_obj_align(t1, LV_ALIGN_LEFT_MID, LV_SCALE(10), LV_SCALE(1));
+    lv_label_set_text(t1, "On");
+    lv_obj_set_size(t1, LV_SCALE(70), LV_SIZE_CONTENT);
+    lv_obj_set_style_text_align(t1, LV_TEXT_ALIGN_RIGHT, 0);
+    lv_obj_set_style_text_font(t1, LV_FONT_GET(14), 0);
   } else {
     // lv_obj_set_size(timerc, dropdown_width, 76);
 
